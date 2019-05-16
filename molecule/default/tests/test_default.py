96cdb2087a1048bcc4b87e2fdcb202e418b0654b
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2019-05-16 08:49:24 +0200

    Major overhaul of the role
    * Added Molecule tests to verify the role (thanks to @geerlingguy for
    the inspiration on your blog and the docker images you provide!)
    * Streamlined the installation process of packages and made it more
    OS-agnostic
    * It is now possible to join an arbitrary amount of domains (all config
    has changed, please check your config!)

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
-
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+def test_ad_user(host):
+    usergroup = 'domain users'
+    users = [{'name': 'dom1usr', 'uid': 1277201126},
+             {'name': 'dom2usr', 'uid': 1647801103}]
+    for user in users:
+        testuser = host.user(user['name'])
+        assert testuser.name == user['name']
+        assert testuser.group == usergroup
+        assert testuser.uid == user['uid']

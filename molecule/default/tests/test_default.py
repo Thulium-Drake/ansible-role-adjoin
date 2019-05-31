7f94aa036ca4a12ee6da387046d6a73a345ec6e5
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2019-05-31 20:40:45 +0200

    Updated test with new UID

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,7 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_ad_user(host):
     usergroup = 'domain users'
-    users = [{'name': 'dom1usr', 'uid': 742801101},
+    users = [{'name': 'dom1usr', 'uid': 380801102},
              {'name': 'dom2usr', 'uid': 742801101}]
     for user in users:
         testuser = host.user(user['name'])

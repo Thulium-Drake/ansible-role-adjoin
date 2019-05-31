4eb2ab1c8155adc3826a1b361696844bfbffa60c
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2019-05-31 18:44:34 +0200

    Added cleanup step

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -8,8 +8,8 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 def test_ad_user(host):
     usergroup = 'domain users'
-    users = [{'name': 'dom1usr', 'uid': 1277201126},
-             {'name': 'dom2usr', 'uid': 1647801103}]
+    users = [{'name': 'dom1usr', 'uid': 742801101},
+             {'name': 'dom2usr', 'uid': 742801101}]
     for user in users:
         testuser = host.user(user['name'])
         assert testuser.name == user['name']

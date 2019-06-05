ba965d9dee3d81812d8623ba6cb968877748e518
Jeffrey van Pelt <jeffrey.vanpelt@element-networks.nl>
2019-06-05 09:52:15 +0200

    * Fixed sudoers template * Added tests for sudo

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -7,11 +7,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def test_ad_user(host):
-    usergroup = 'domain users'
-    users = [{'name': 'dom1usr', 'uid': 380801102},
-             {'name': 'dom2usr', 'uid': 742801101}]
+    users = [{'name': 'dom1usr', 'uid': 380801102, 'group': 'dom1_lnx_admins'},
+             {'name': 'dom2usr', 'uid': 742801101, 'group': 'dom2_lnx_admins'}]
     for user in users:
         testuser = host.user(user['name'])
+
+        # Test if the domain users/groups are present
         assert testuser.name == user['name']
-        assert testuser.group == usergroup
+        assert user['group'] in testuser.groups
         assert testuser.uid == user['uid']
+
+        # Test if the user can run sudo
+        with host.sudo():
+            assert host.check_output("whoami") == 'root'
